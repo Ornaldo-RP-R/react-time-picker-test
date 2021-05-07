a130d22f24f3630a9a43daf3eced8904fe9335fe
crs <ornaldo.hasa@fshnstudent.info>
2021-05-07 16:53:31 +0200

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import TimeInput from "react-time-picker-input"
+function App(props) {
+  const [date,setDate]=useState('10:12')
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <TimeInput hour12Format value={date} onChange={(dateString)=>{
+     }}/>
     </div>
   );
 }
 
+   
 export default App;
