234adf5f885550005e708fe583021e00dce025b0
crs <ornaldo.hasa@fshnstudent.info>
2021-05-10 16:53:00 +0200

    m

---
+++ src/components/AmPmInputHelper.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AmPmInputHelper = (props) => {
+  const { amPm, focusMinuteInput, toggleAmPm, setAmPM, inputRef, ...otherProps } = props;
+  return (
+    <input
+      id="react-time-input-picker__amPm"
+      type="text"
+      {...otherProps}
+      value={amPm}
+      ref={inputRef}
+      readOnly
+      onKeyDown={(e) => {
+        if (e.key === "ArrowLeft") {
+          focusMinuteInput();
+        }
+        if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+          toggleAmPm();
+        }
+        if (e.key.toLocaleLowerCase() === "p") {
+          setAmPM("PM");
+        }
+        if (e.key.toLocaleLowerCase() === "a") {
+          setAmPM("AM");
+        }
+      }}
+    />
+  );
+};
+
+export default AmPmInputHelper;
