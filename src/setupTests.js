29c327a6b0c9f6587b8f038e790fe6762198dcf4
crs <ornaldo.hasa@fshnstudent.info>
2023-07-17 18:16:48 +0200

    m

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import TimeInput from "react-time-picker-input";
+import TimeInput from "react-time-picker-input/dist/index";
 function TimeInputEx() {
   const [dateEx7, setDateEx7] = useState("10:12");
   return <TimeInput value={dateEx7} hour12Format eachInputDropdown onChange={(dateString) => setDateEx7(dateString)} />;
